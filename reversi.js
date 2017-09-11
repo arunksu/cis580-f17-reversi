f2835e6dcfb4935171d95cb067e067a1948d3e7f
Arun P <arunparamanathan@ksu.edu>
2017-09-11 17:49:15 -0500

    beginning move algorithm
    beginning move algorithm

--- reversi.js
+++ reversi.js
@@ -7,7 +7,8 @@ var b = 'b';
 var w = 'w';
 
 // All info about game state.
-var state = {
+var state =
+{
   gameOver: false,
   turn: b,
   board: [
@@ -22,55 +23,70 @@ var state = {
   ]
 }
 
-function handlePieceClick(event) {
-  event.preventDefault();
-  var parentId = event.target.parentElement.id;
-  var x = parseInt(parentId.charAt(7));
-  var y = parseInt(parentId.charAt(9));
-  var piece = state.board[y][x];
+function getPossibleMoves()
+{
+  var moves = [];
+  var startPositions = [];
 
-  // Clear old highlights
-  clearHighlights();
+  // Get either all black pieces or white pieces on board.
+  var piecesForTurn = [];
+  if (state.turn === b) { piecesForTurn = document.querySelectorAll('.piece-b'); }
+  else { piecesForTurn = document.querySelectorAll('.piece-w'); }
 
-  // Make sure the checker is the player's
-  if(piece.charAt(0) !== state.turn) return;
+  for (i = 0; i < piecesForTurn.length; i++)
+  {
+    var x = piecesForTurn[i].parentElement.id.charAt(7);
+    var y = piecesForTurn[i].parentElement.id.charAt(9);
 
-  // mark checker to move
-  event.target.classList.add('highlight');
+    // Store all squares with pieces on them that we
+    // can draw lines out from. These lines will end
+    // on whatever squares the player can place a piece on.
+    startPositions.push({x: x, y: y});
+  }
+
+  // Return all squares we can place piece on.
+  return moves;
 }
 
-function clearHighlights() {
+function clearHighlights()
+{
   var highlighted = document.querySelectorAll('.highlight');
-  highlighted.forEach(function(elem) {
+  highlighted.forEach(function(elem)
+  {
     elem.classList.remove('highlight');
   });
 }
 
-function setup() {
+function setup()
+{
   // Create game board.
   var board = document.createElement('section');
   board.id = 'game-board';
   document.body.appendChild(board);
 
   // Add square info.
-  for(var y = 0; y < state.board.length; y++) {
-    for(var x = 0; x < state.board[y].length; x++) {
+  for(var y = 0; y < state.board.length; y++)
+  {
+    for(var x = 0; x < state.board[y].length; x++)
+    {
       var square = document.createElement('div');
       square.id = "square-" + x + "-" + y;
       square.classList.add('square');
       board.appendChild(square);
 
       // Add pieces on squares that have them.
-      if(state.board[y][x]) {
+      if(state.board[y][x])
+      {
         var piece = document.createElement('div');
         piece.classList.add('piece');
         // piece-w or piece-b class to signify color.
         piece.classList.add('piece-' + state.board[y][x]);
-        piece.onclick = handlePieceClick;
         square.appendChild(piece);
       }
     }
   }
+  // Let us start the game.
+  getPossibleMoves();
 }
 
 setup();
